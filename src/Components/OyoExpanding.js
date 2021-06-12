88ffecce5f05bf60710bcabfb6fe91b6747b972b
namangilly <anshumank72@gmail.com>
2021-06-12 20:24:25 +0530

    completed map section

--- src/Components/OyoExpanding.js
+++ src/Components/OyoExpanding.js
@@ -2,6 +2,11 @@ import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 const useStyles = makeStyles((theme) => ({
   fadIncontainer: {
@@ -16,9 +21,8 @@ const useStyles = makeStyles((theme) => ({
     margin: "0 auto",
   },
   oyoCell: {
-    margin: "0 auto",
     position: "relative",
-    paddingRight: "40px",
+    margin: "0 60px 0 20px",
     maxWidth: "720px",
     maxHeight: "408px",
     boxSizing: "border-box",
@@ -29,6 +33,11 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     justifyContent: "center",
     boxSizing: "border-box",
+  },
+  separator: {
+    fontSize: "70px",
+    fontWeight: "100",
+    padding: "0 8px",
   },
 }));
 
@@ -174,16 +183,18 @@ const oyoExpanding = (props) => {
             <img
               src="https://assets.oyoroomscdn.com/cmsMedia/432737_widgetImage.jpg"
               alt="map photo"
+              style={{}}
             />
           </div>
           <div className={classes.oyoCellColumn}>
             <Typography
               variant="h3"
               style={{
-                fontSize: "1.6rem",
+                fontSize: "1.8rem",
                 fontWeight: "700",
                 color: "#333",
                 fontFamily: "Open Sans,sans-serif",
+                padding: "20px 0",
               }}
             >
               World's leading chain of hotels and homes
@@ -199,26 +210,93 @@ const oyoExpanding = (props) => {
                 marginTop: "0",
                 fontWeight: "200",
                 fontFamily: "Open Sans,sans-serif",
+                padding: "20px 0",
               }}
             >
               More Destination. More Ease. More Affordable
             </Typography>
             <div className={classes.oyoCount}>
-              <Breadcrumbs aria-label="breadcrumb">
+              <Breadcrumbs
+                aria-label="breadcrumb"
+                classes={{ root: classes.root, separator: classes.separator }}
+              >
                 <div>
-                  <Typography variant="h4">80</Typography>
-                  <Typography variant="subtitle1">Countries</Typography>
+                  <Typography
+                    variant="h4"
+                    style={{ fontWeight: 500, color: "#333" }}
+                  >
+                    80
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ fontWeight: 100 }}>
+                    Countries
+                  </Typography>
                 </div>
                 <div>
-                  <Typography variant="h4">4300+</Typography>
-                  <Typography variant="subtitle1">Hotels</Typography>
+                  <Typography
+                    variant="h4"
+                    style={{ fontWeight: 500, color: "#333" }}
+                  >
+                    4300+
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ fontWeight: 100 }}>
+                    Hotels
+                  </Typography>
                 </div>
                 <div>
-                  <Typography variant="h4">150,000</Typography>
-                  <Typography variant="subtitle1">Vacation homes</Typography>
+                  <Typography
+                    variant="h4"
+                    style={{ fontWeight: 500, color: "#333" }}
+                  >
+                    150,000
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ fontWeight: 100 }}>
+                    Vacation homes
+                  </Typography>
                 </div>
               </Breadcrumbs>
             </div>
+            <List component="ul" aria-label="main countries folders">
+              <div style={{ display: "flex" }}>
+                <ListItem>
+                  <ListItemIcon>
+                    <FiberManualRecordIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="India" />
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <FiberManualRecordIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="China" />
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <FiberManualRecordIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Malaysia" />
+                </ListItem>
+              </div>
+              <div style={{ display: "flex" }}>
+                <ListItem>
+                  <ListItemIcon>
+                    <FiberManualRecordIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Nepal" />
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <FiberManualRecordIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Indonesia" />
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <FiberManualRecordIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="UAE" />
+                </ListItem>
+              </div>
+            </List>
           </div>
         </div>
       </div>
